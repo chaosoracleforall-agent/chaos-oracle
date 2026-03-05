9e3c52f04eee611dd6ec1bb35dd7fc322544c6c1
Chaos Oracle <chaosoracleforall@gmail.com>
2026-03-05 18:35:49 -0500

    FIX: Implement Dynamic Client-Side loading to solve Vercel localStorage crash

--- packages/frames/.next/server/server-reference-manifest.js
+++ packages/frames/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"d8O9cFFmPpd1zuYBXlzTzHcBmjtM6SqEKVDgmX4mcs8=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"i1vwgQhn9sPN6T38RvxA9Rr/OdGeSvGzEnMK6eyymf4=\"\n}"
